bfb4f97949af88716b026d1878ef56f6d6b41827
michouse <michouse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-11 12:18:19 +0000

    Adding first version fo wp-extend-toolbar
    git-svn-id: https://plugins.svn.wordpress.org/wp-extend-toolbar/trunk@1196979 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== WP Extend Toolbar ===
+Contributors: michouse
+Tags: plugin, admin bar, tool bar, custom admin bar, custom tool bar
+Requires at least: 4.0
+Tested up to: 4.2.2
+Stable tag: 0.1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP Extend Toolbar is display the following page information to the admin bar.
+
+
+== Description ==
+
+WP Extend Toolbar is display the following page information to the admin bar.
+
+* content of Title tag.
+* content of Description tag.
+
+WP Extend Toolbar は、ツールバーを拡張し、以下のページ情報を表示します。
+
+* Title タグの内容
+* Description タグの内容
+
+
+== Installation ==
+
+1. Upload `WP Extend Toolbar` to the `/wp-content/plugins/` directory
+1. Activate the plugin
+1. Adds a page information to the admin bar.
+
+
+== Screenshots ==
+
+1. Example.
+
+
+== Changelog ==
+
+= 0.1.0 =
+* Initial release.
---
+++ wp-extend-toolbar.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ Plugin Name: WP Extend Toolbar
+ Plugin URI: https://github.com/michouse/wp-extend-toolbar
+ Description: Adds a page information to the admin bar.
+ Author: Michinari Odajima
+ Version: 0.1.0
+ Author URI: http://piece-web.jp/
+ Domain Path: /language
+ Text Domain: wp-extend-toolbar
+ */
+
+class WP_Extend_Toolbar {
+
+	function WP_Extend_Toolbar() {
+		
+		add_action( 'admin_bar_init', array( $this, 'admin_bar_init' ) );
+		
+	}
+	
+	function admin_bar_init() {
+
+		if ( ! is_admin() && is_admin_bar_showing() && is_user_logged_in() ) {
+
+			add_action( 'admin_bar_menu', array( $this, 'admin_bar_menu' ), 100 );
+			
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ), 100 );
+		
+		}
+	}
+	
+	function admin_bar_menu() {
+		
+		global $wp_admin_bar;
+		
+		$wp_admin_bar->add_group( array(
+			'id'     => 'extend-toolbar',
+			'meta'   => array(
+				'class' => 'ab-extend-toolbar',
+			),
+		) );
+		
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'extend-toolbar',
+			'id'     => 'extend-toolbar-title',
+			'title'  => '',
+			'meta'   => array(),
+		) );
+		
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'extend-toolbar',
+			'id'     => 'extend-toolbar-description',
+			'title'  => '',
+			'meta'   => array(),
+		) );
+		
+	}
+	
+	function enqueue() {
+		
+		wp_enqueue_style( 'wp-extend-toolbar', plugins_url( "css/wp-extend-toolbar.css", __FILE__ ), array(), '0.1.0' );
+
+		wp_enqueue_script( 'wp-extend-toolbar', plugins_url( "js/wp-extend-toolbar.js", __FILE__ ), array( 'jquery' ), '0.1.0', true );
+		
+	}
+}
+
+$GLOBALS['wp_extend_toolbar'] = new WP_Extend_Toolbar();
